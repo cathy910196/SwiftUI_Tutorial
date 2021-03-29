5c274e5504d9cdf743283f0006a3d1ac31e686a8
cathyke <cathy910196@gmail.com>
2021-03-29 12:27:02 +0800

    first commit

---
+++ Shared/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  Shared
+//
+//  Created by mdspc on 2021/3/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
